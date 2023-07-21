acd0b2cfca3c1f7763a2cc294cc08300f6a6a0ff
davidingles <davidingleslopez@gmail.com>
2023-07-21 14:27:36 +0200

    no funciona del tobo bien el la pagina dinámica pero no te rindas

--- src/data/catalogo.js
+++ src/data/catalogo.js
@@ -1,18 +1,18 @@
 export const catalogo = [
     {
-        id: 1,
+        id: '1',
         nombre: 'b1-cerrada',
         precio: 100,
         imagen: '/cajas/3D BANG CON FLAYERS.png'
     },
     {
-        id: 2,
+        id: '2',
         nombre: 'b1-abierta',
         precio: 100,
         imagen: '/cajas/3D CHERRY.png'
     },
     {
-        id: 3,
+        id: '3',
         nombre: 'bandeja',
         precio: 50,
         imagen: '/cajas/3D CUBREALARMAS.png'
