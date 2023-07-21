5605de4e683e5f10d76bb2f38a6ee2960c59c15d
davidingles <davidingleslopez@gmail.com>
2023-07-21 11:04:23 +0200

    usando un solo archivo y conseguido

--- src/components/three/Model.jsx
+++ src/components/three/Model.jsx
@@ -1,13 +1,48 @@
 import { ContactShadows, Environment, OrbitControls, Stage } from '@react-three/drei'
 import { Canvas } from '@react-three/fiber'
 import { Suspense } from 'react'
-import { Dav } from './Dav'
-import styles from './Index.module.css'
+// import { Dav } from './Dav'
+import * as THREE from "three";
+import { useGLTF } from "@react-three/drei";
+
+
+export function Dav(props) {
+  const { nodes, materials } = useGLTF("/models/B1_cerrada.gltf");
+  return (
+    <>
+      
+      <group {...props} dispose={null}>
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes["TQ_ARD-geom001"].geometry}
+          material={materials["TQ_ARD_front.001"]}
+          name="meshPhongMaterial"
+          // material={new THREE.MeshPhongMaterial( { color: 0xff0000 } )}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes["TQ_ARD-geom001_1"].geometry}
+          material={materials["TQ_ARD_back.001"]}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes["TQ_ARD-geom001_2"].geometry}
+          material={materials["TQ_ARD_edge.001"]}
+        />
+      </group>
+    </>
+  );
+}
+
+useGLTF.preload("/models/B1_cerrada.gltf");
 
 const Model = () => {
 
   return (
-        <div className={styles.canvas} style={{backgroundColor: 'rgba(0,0,0,0)'}}>
+        <div style={{backgroundColor: 'rgba(0,0,0,0)'}}>
         
           <Canvas
             className='shadow-xl'
@@ -27,11 +62,19 @@ const Model = () => {
             <pointLight position={[10, 10, 10]} />
             <Suspense fallback={null}>
         
-                <Dav
+              <Dav/>
+
+                {/* <Dav
                   scale={.5}
                   position={[-.0,0.1,-0.00]}
                   rotation={[Math.PI / 1000, 0, 0]}
-                />
+                >
+                  <group>
+                    <mesh material={new THREE.MeshPhongMaterial( { color: 0xff00ff } )}/>
+                    <mesh/>
+                    <mesh/>
+                  </group>
+                </Dav> */}
         
             </Suspense>
             <OrbitControls autoRotate autoRotateSpeed={1} />
