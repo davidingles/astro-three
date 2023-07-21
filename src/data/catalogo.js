064da16be40b7b36f5b0a9376c5d9e755ae86102
davidingles <davidingleslopez@gmail.com>
2023-07-21 13:01:46 +0200

    añado un boton2

---
+++ src/data/catalogo.js
@@ -0,0 +1,20 @@
+export const catalogo = [
+    {
+        id: 1,
+        nombre: 'b1-cerrada',
+        precio: 100,
+        imagen: '/cajas/3D BANG CON FLAYERS.png'
+    },
+    {
+        id: 2,
+        nombre: 'b1-abierta',
+        precio: 100,
+        imagen: '/cajas/3D CHERRY.png'
+    },
+    {
+        id: 3,
+        nombre: 'bandeja',
+        precio: 50,
+        imagen: '/cajas/3D CUBREALARMAS.png'
+    },
+]
